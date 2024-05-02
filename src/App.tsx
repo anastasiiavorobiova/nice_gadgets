cdd677f38ac16fe741d1ce941c66b8879f23f1ff
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-05-02 15:11:04 +0300

    routing init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Counter from './components/Counter';
+import { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { CartLazy } from './pages/Cart/CartLazy';
+import { FavouritesLazy } from './pages/Favourites/FavouritesLazy';
+import { HomeLazy } from './pages/Home/HomeLazy';
+
 import './index.scss';
 
 export const App = () => {
 	return (
 		<div className="app">
-			<Counter />
+			<div>
+				<Link to={'/'}>Home</Link>
+				<Link to={'/cart'}>Cart</Link>
+				<Link to={'/favourites'}>Favourites</Link>
+			</div>
+			<Suspense fallback={<div>Loading..</div>}>
+				<Routes>
+					<Route path="/cart" element={<CartLazy />} />
+					<Route path="/favourites" element={<FavouritesLazy />} />
+					<Route path="/" element={<HomeLazy />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
