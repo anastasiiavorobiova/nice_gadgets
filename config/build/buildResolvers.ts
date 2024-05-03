5279c2e5ee6120206e3af4a16cd6b8c7262a56d9
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-05-03 14:51:16 +0300

    fix build config absolute path, modules, mainFile

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -4,6 +4,9 @@ import { BuildOptions } from './types/config';
 export function buildResolvers({ paths }: BuildOptions): ResolveOptions {
 	return {
 		extensions: ['.tsx', '.ts', '.js'],
+		preferAbsolute: true,
+		modules: [paths.src, 'node_modules'],
+		mainFiles: ['index'],
 		alias: {
 			'@': [paths.src],
 		},
