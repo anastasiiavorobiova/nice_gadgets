5dfcd4cef83dd0b8db69159b3ae7ff6b6c2fd129
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-05-04 13:45:26 +0300

    refactor applink, theme removed

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -3,27 +3,18 @@ import { NavLink, NavLinkProps } from 'react-router-dom';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import * as cls from './AppLink.module.scss';
 
-export enum AppLinkTheme {
-	ORIGINAL = 'original',
-	DARK = 'dark',
-	BLUE = 'rounded-blue',
-	ORANGE = 'rounded-orange',
-	PURPLE = 'rounded-purple',
-}
-
 interface AppLinkProps extends NavLinkProps {
 	className?: string;
-	theme?: AppLinkTheme;
 }
 
-const getActiveLink = (className: string, theme: string) => {
+const getActiveLink = (className: string) => {
 	return ({ isActive }: { isActive: boolean }) => {
 		return classNames(
 			cls.AppLink,
 			{
 				[cls.active]: isActive,
 			},
-			[className, cls[theme]]
+			[className]
 		);
 	};
 };
@@ -32,15 +23,10 @@ export const AppLink: FC<AppLinkProps> = ({
 	className,
 	children,
 	to,
-	theme = AppLinkTheme.ORIGINAL,
 	...otherProps
 }) => {
 	return (
-		<NavLink
-			to={to}
-			className={getActiveLink(className, theme)}
-			{...otherProps}
-		>
+		<NavLink to={to} className={getActiveLink(className)} {...otherProps}>
 			{children}
 		</NavLink>
 	);
