652d868c1f5e839f074d2b26a714c284e62d9a5c
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-05-04 17:17:02 +0300

    sidebar added

---
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -0,0 +1,25 @@
+import { FC, useState } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import * as cls from './SideBar.module.scss';
+
+interface SideBarProps {
+	className?: string;
+}
+
+export const SideBar: FC<SideBarProps> = ({ className }) => {
+	const [collapsed, setCollapsed] = useState(false);
+
+	return (
+		<div
+			className={classNames(cls.SideBar, { [cls.collapsed]: collapsed }, [
+				className,
+			])}
+		>
+			SideBar
+			<br />
+			<br />
+		</div>
+	);
+};
+
+export default SideBar;
